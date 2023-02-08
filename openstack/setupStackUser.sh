0034523aa1e0a91871f97699ea7090d3b86fea2c
MRColor <mattiar11@gmail.com>
2023-02-08 14:50:03 +0100

    added setupstack user

---
+++ openstack/setupStackUser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update && sudo apt full-upgrade -y
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
