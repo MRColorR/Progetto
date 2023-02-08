58809eb53a50319cc0f50223558b6d151c7fb0fe
MRColor <mattiar11@gmail.com>
2023-02-08 15:32:58 +0100

    fix read float range

--- openstack/setupAIO.sh
+++ openstack/setupAIO.sh
@@ -9,7 +9,7 @@ sed -i "s^FLAT_INTERFACE=eth1^FLAT_INTERFACE=$IFACE_NAME^" $PWD/devstack/local.c
 IFACE_IP=$(ip addr show $IFACE_NAME | grep "inet\b" | awk '{print $2}' |  cut -d/ -f1)
 sed -i "s^HOST_IP=192.168.1.111^HOST_IP=$IFACE_IP^" $PWD/devstack/local.conf
 FLOAT_RANGE="192.168.1.112/28"
-FLOAT_RANGE=read -r -p "Insert the floating ip range to use on the interface $IFACE_NAME that has ip $IFACE_IP , a /27 or /28 will suffice e.g. insert 192.168.1.112/28"$'\n';
+read -r -p "Insert the floating ip range to use on the interface $IFACE_NAME that has ip $IFACE_IP , a /27 or /28 will suffice e.g. insert 192.168.1.112/28"$'\n' FLOAT_RANGE;
 sed -i "s^192.168.1.112/28^$FLOAT_RANGE^" $PWD/devstack/local.conf
 cp $PWD/onehundredten/openstack/myprepare.sh . && sudo chmod +x myprepare.sh && ./myprepare.sh
 
