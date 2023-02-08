064f7a65e52a1f667287517f8b903e3f7cb711dd
MRColor <mattiar11@gmail.com>
2023-02-08 18:32:23 +0100

    added checks to repeat some stages if fails

--- openstack/setupAIO.sh
+++ openstack/setupAIO.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sudo apt install git -y && git clone https://opendev.org/openstack/devstack
+sudo apt install git -y
+while true; do
+git clone https://opendev.org/openstack/devstack
+if [ $? -eq 0 ]; then
+break
+fi
+done
 cp $PWD/onehundredten/openstack/mylocal.conf $PWD/devstack/local.conf
 
 read -r -p "Insert the password you want to assign to the openstack admin"$'\n' PASSWD ;
@@ -11,7 +17,16 @@ sed -i "s^HOST_IP=192.168.1.111^HOST_IP=$IFACE_IP^" $PWD/devstack/local.conf
 FLOAT_RANGE="192.168.1.112/28"
 read -r -p "Insert the floating ip range to use on the interface $IFACE_NAME that has ip $IFACE_IP , a /27 or /28 will suffice e.g. insert 192.168.1.112/28"$'\n' FLOAT_RANGE;
 sed -i "s^192.168.1.112/28^$FLOAT_RANGE^" $PWD/devstack/local.conf 
+
+while true; do
 $PWD/devstack/stack.sh
+if [ $? -eq 0 ]; then
+break
+fi
+$PWD/devstack/unstack.sh
+$PWD/devstack/clean.sh
+done
+
 cp $PWD/onehundredten/openstack/myprepare.sh . && sudo chmod +x myprepare.sh && ./myprepare.sh
 
 if [ $? -eq 0 ]; then
