e80887575e7c9ab0904fcbcb0738bc01b2efadad
MRColor <mattiar11@gmail.com>
2023-02-08 15:51:51 +0100

    fixed bad directory change

--- openstack/setupAIO.sh
+++ openstack/setupAIO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install git -y && git clone https://github.com/openstack/devstack
+#sudo apt install git -y && git clone https://github.com/openstack/devstack
 cp $PWD/onehundredten/openstack/mylocal.conf $PWD/devstack/local.conf
 
 read -r -p "Insert the password you want to assign to the openstack admin"$'\n' PASSWD ;
@@ -10,9 +10,8 @@ IFACE_IP=$(ip addr show $IFACE_NAME | grep "inet\b" | awk '{print $2}' |  cut -d
 sed -i "s^HOST_IP=192.168.1.111^HOST_IP=$IFACE_IP^" $PWD/devstack/local.conf
 FLOAT_RANGE="192.168.1.112/28"
 read -r -p "Insert the floating ip range to use on the interface $IFACE_NAME that has ip $IFACE_IP , a /27 or /28 will suffice e.g. insert 192.168.1.112/28"$'\n' FLOAT_RANGE;
-sed -i "s^192.168.1.112/28^$FLOAT_RANGE^" $PWD/devstack/local.conf
-cd /opt/stack/devstack/ 
-./stack && cd ..
+sed -i "s^192.168.1.112/28^$FLOAT_RANGE^" $PWD/devstack/local.conf 
+$PWD/devstack/stack.sh
 cp $PWD/onehundredten/openstack/myprepare.sh . && sudo chmod +x myprepare.sh && ./myprepare.sh
 
 if [ $? -eq 0 ]; then
