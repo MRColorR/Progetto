d915ae3883ec7d902b388a6d092c980dc5b4fcf8
MRColor <mattiar11@gmail.com>
2023-02-14 11:32:13 +0100

    new boxplot script added nrequirements

--- K8s-metrics/draw_metrics.py
+++ K8s-metrics/draw_metrics.py
@@ -1,35 +1,44 @@
-import csv
+import argparse
+import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_metrics(filename):
+def main():
     """
-    Plot the CPU usage and memory usage metrics stored in a CSV file.
-    
-    Parameters:
-        filename (str): The name of the CSV file.
+    Main function to draw boxplots of the CPU and memory usage.
     """
-    # Read the data from the CSV file
-    cpu_usage = []
-    memory_usage = []
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            cpu_usage.append(float(row["cpu_usage_avg"]))
-            memory_usage.append(float(row["memory_usage_avg"]))
+    # Define the command-line arguments
+    parser = argparse.ArgumentParser(description="Draw boxplots of the CPU and memory usage")
+    parser.add_argument("--filename", type=str, default="deployment_metrics.csv", help="The input file name")
+    args = parser.parse_args()
+
+    # Load the data from the CSV file
+    data = pd.read_csv(args.filename)
     
-    # Plot the CPU usage data as a box plot
-    plt.figure()
-    plt.boxplot(cpu_usage)
-    plt.title("CPU Usage")
-    plt.ylabel("Usage (mHz)")
-    plt.savefig("cpu_usage_boxplot.svg")
+    # Get the unique values of the hpa_cpu_threshold
+    unique_thresholds = data["hpa_cpu_threshold"].unique()
     
-    # Plot the memory usage data as a box plot
-    plt.figure()
-    plt.boxplot(memory_usage)
-    plt.title("Memory Usage")
-    plt.ylabel("Usage (MB)")
-    plt.savefig("memory_usage_boxplot.svg")
+    # Loop over the unique values of the hpa_cpu_threshold
+    for hpa_cpu_threshold in unique_thresholds:
+        # Filter the data for the current hpa_cpu_threshold
+        filtered_data = data[data["hpa_cpu_threshold"] == hpa_cpu_threshold]
+        
+        # Draw the boxplots of the CPU and memory usage
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        filtered_data.boxplot(column="cpu_usage_avg", ax=ax[0])
+        filtered_data.boxplot(column="memory_usage_avg", ax=ax[1])
+        
+        # Add the hpa_cpu_threshold to the x axis of the boxplots
+        ax[0].axhline(y=hpa_cpu_threshold, color='r', linestyle='--')
+        ax[1].axhline(y=hpa_cpu_threshold, color='r', linestyle='--')
+        
+        # Set the titles of the boxplots
+        ax[0].set_title("CPU Usage")
+        ax[1].set_title("Memory Usage")
+        
+        # Save the figure as a SVG file
+        postfix = "_threshold_{}".format(int(hpa_cpu_threshold))
+        plt.savefig("deployment_metrics{}.svg".format(postfix))
 
 if __name__ == "__main__":
-    plot_metrics("deployment_metrics.csv")
+    main()
+
